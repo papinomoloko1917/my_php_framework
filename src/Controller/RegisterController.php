e0b08a7eb0e30ac17bbf3cf7663d0da904077fac
ninjamax1917 <ninjamax177@gmail.com>
2026-05-06 16:29:44 +0300

    update

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,19 +4,35 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use RuntimeException;
-
 class RegisterController extends Controller {
     public function showForm(): string {
         return $this->view->page('register');
     }
     public function register(): ?string {
-        $email = $this->request->input('email');
+        $rawEmail = $this->request->input('email');
+        $rawPassword = $this->request->input('password');
+        $email = htmlspecialchars(trim($rawEmail), ENT_QUOTES, 'UTF-8');
+        $password = password_hash($rawPassword, PASSWORD_DEFAULT);
         $error = [];
-        if (!empty($email)) {
-            return $email;
+        if (!empty($rawEmail)) {
+            if (filter_var($rawEmail, FILTER_VALIDATE_EMAIL)) {
+                return $email;
+            } else {
+                $error[] = 'Некорректный формат Email';
+            }
+        } else {
+            $error[] = 'Email не может быть пустым';
         }
-        $error[] = 'Email не передан';
+
+        if (!empty($rawPassword)) {
+            if (!strlen($rawPassword) < 6) {
+                return $password;
+            } else {
+                $error[] = 'Количество символов в пароле должно быть больше 6';
+            }
+        } else {
+            $error[] = 'Пароль не может быть пустым';
+        }
         return implode('<br>', $error);
     }
 }
